0806ad023e1d54234a59f7b853becbc647c94bc2
Nirit Nagar <nirit.nn@gmail.com>
2023-02-14 22:32:08 +0100

    update avatars url

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,54 @@
-import React, { useState } from 'react';
-import { AppProvider } from './Contexts/AppCntxt'
-import './App.css';
-import List from './Components/List';
-import FormList from './Components/FormList';
+import React, { useState } from "react";
+import { AppProvider } from "./Contexts/AppCntxt";
+import "./App.css";
+import List from "./Components/List";
+import FormList from "./Components/FormList";
 
 export interface IState {
   users: {
-    img: string,
-    name: string,
-    position: string
-    note?: string
-  }[]
+    img: string;
+    name: string;
+    position: string;
+    note?: string;
+  }[];
 }
-
 
 export const avatarUrl = {
-  young: "https://nirit.website/projects/react-typescript-context/img/avatar5.png",
-  female: "https://nirit.website/projects/react-typescript-context/img/avatar10.png",
-  adult: "https://nirit.website/projects/react-typescript-context/img/avatar2.png",
-  other: "https://nirit.website/projects/react-typescript-context/img/avatar4.png",
-  unique: "https://nirit.website/projects/react-typescript-context/img/avatar13.png"
-}
-
-
+  young: "https://ibb.co/YfB4QnM",
+  female: "https://ibb.co/RTT2Dm3",
+  adult: "https://ibb.co/Wc6BTVx",
+  other: "https://ibb.co/sqSX2fb",
+  unique: "https://ibb.co/zfb7kpt",
+};
 
 function App() {
-
-
   const [users, setUsers] = useState<IState["users"]>([
     {
       name: "Jonas Kernn",
       position: "Frontend Engineer",
       img: avatarUrl.other,
-      note: "Loves weekends..😉!"
+      note: "Loves weekends..😉!",
     },
     {
       name: "Morin Levi",
       position: "Backend Engineer",
       img: avatarUrl.young,
-      note: "Addicted to smoothies..🥤"
-    }
-  ])
-
-
-
+      note: "Addicted to smoothies..🥤",
+    },
+  ]);
 
   return (
     <>
       <AppProvider>
         <div className="app">
-            <div className="header">
-              <FormList setUsers={setUsers} users={users} maxlength={20} />
-              <List users={users} />
-            </div>
+          <div className="header">
+            <FormList setUsers={setUsers} users={users} maxlength={20} />
+            <List users={users} />
+          </div>
         </div>
       </AppProvider>
-
-        </>
+    </>
   );
 }
-
 
 export default App;
--- src/Components/FormList.tsx
+++ src/Components/FormList.tsx
@@ -1,162 +1,190 @@
-import React, { useState, useContext } from 'react'
-import { AppContext } from '../Contexts/AppCntxt'
+import React, { useState, useContext } from "react";
+import { AppContext } from "../Contexts/AppCntxt";
 import { IState as Props, avatarUrl } from "../App";
-import TeamDone from './TeamDone';
-
+import TeamDone from "./TeamDone";
 
 interface IProps {
-    setUsers: React.Dispatch<React.SetStateAction<Props["users"]>>
-    users: Props["users"]
-    maxlength: number
+  setUsers: React.Dispatch<React.SetStateAction<Props["users"]>>;
+  users: Props["users"];
+  maxlength: number;
 }
 
 const FormList: React.FC<IProps> = ({ setUsers, users, maxlength }) => {
+  let { counTeam, setCounTeam } = useContext(AppContext);
+  const { showForm } = useContext(AppContext);
 
-   
-    let { counTeam, setCounTeam } = useContext(AppContext);
-    const { showForm } = useContext(AppContext);
+  const [error, setError] = useState<boolean>(false);
 
-    const [error, setError] = useState<boolean>(false)
+  const [input, setInput] = useState({
+    img: "../Img/avatar5.png",
+    name: "",
+    position: "",
+    note: "",
+  });
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const [input, setInput] = useState({
-        img: "https://nirit.website/projects/react-typescript-context/img/avatar5.png",
-        name: "",
-        position: "",
-        note: ""
-    })
-    
-   
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value
-        })
-    }
-   
-
-
-    const checkForm = () => {
-
-        if (input.img === "" || input.name === "" || input.position === "" || input.note === "")        
-        setError(true)
-         else
-            formOk()
-    }
-        
-        
-    const formOk = () => {
-
-            if (users.length < 5) 
-
-                if (!input.img || !input.name || !input.position || !input.note)
-                                       
-            return
-
-                setError(false)
-                setCounTeam(counTeam = counTeam + 1);
-        
-
-                setUsers([
-                    ...users,
-                    {
-                        name: input.name,
-                        position: input.position,
-                        img: input.img,
-                        note: input.note
-                    }
-                ]);
-
-            setInput({
-                    img: input.img,
-                    name: "",
-                    position: "",
-                    note: ""
-            })
-            
-   }
-
-    
-
-    return (
-        <>
-            <div className={showForm ? "team-done done-hide" : "team-done done-show"}>
-                 <TeamDone/>
-            </div>
-
-         <div className={showForm ? "list-form form-show" : "list-form inactive form-hide"}>
-            <h1>Create Your Winning Team!</h1>
-            <h2>Within giving you the chance to build your own team (without any limitation whatsoever!)..what your Elite Team would look like?</h2>
-             <p>👇 Fill in the form below to add your top 5 team members:</p>
-            <input 
-                type="text"
-                onChange={handleChange}
-                name="name"
-                value={input.name}
-                placeholder="Name"
-                maxLength={maxlength}
-                required
-            />
-            <input 
-                type="text"
-                onChange={handleChange}
-                name="position"
-                value={input.position}
-                placeholder="Position"
-                maxLength={maxlength}
-                required
-            />
-
-            <h4>Avatar:</h4>
-            <div className="radio-avatar">
-                <div>
-                    <input type="radio" value={avatarUrl.young} name="img" onChange={handleChange} defaultChecked={true}/>
-                    <label htmlFor="Male"><img className="thumb-avatar" alt="" src={avatarUrl.young}/></label>
-                </div>
-
-                <div>
-                    <input type="radio" value={avatarUrl.female} name="img" onChange={handleChange} />
-                    <label htmlFor="Female"> <img className="thumb-avatar" alt="" src={avatarUrl.female}/></label>
-                </div>
-
-                <div>
-                    <input type="radio" value={avatarUrl.adult} name="img" onChange={handleChange} />
-                    <label htmlFor="Other"><img className="thumb-avatar" alt="" src={avatarUrl.adult}/></label>
-                </div>
-
-                <div>
-                    <input type="radio" value={avatarUrl.other} name="img" onChange={handleChange} />
-                    <label htmlFor="Other"><img className="thumb-avatar" alt="" src={avatarUrl.other}/></label>
-                </div>
-
-                <div>
-                    <input type="radio" value={avatarUrl.unique} name="img" onChange={handleChange} />
-                    <label htmlFor="Other"><img className="thumb-avatar" alt="" src={avatarUrl.unique}/></label>
-                </div>
-
-            </div>
-            
-
-            
-            <textarea
-                onChange={handleChange}
-                name="note"
-                value={input.note}
-                placeholder="Short Anecdote"
-                maxLength={maxlength + 25}
-                required
-            />
-
-            <div className={error ? "formError" : "formError hide"}>
-                Please check your entries and try again.
-            </div>
-
-            <button onClick={checkForm}> Add New Team Member</button>
-
-            </div>
-            </>
+  const checkForm = () => {
+    if (
+      input.img === "" ||
+      input.name === "" ||
+      input.position === "" ||
+      input.note === ""
     )
-}
+      setError(true);
+    else formOk();
+  };
 
-export default FormList
+  const formOk = () => {
+    if (users.length < 5)
+      if (!input.img || !input.name || !input.position || !input.note) return;
+
+    setError(false);
+    setCounTeam((counTeam = counTeam + 1));
+
+    setUsers([
+      ...users,
+      {
+        name: input.name,
+        position: input.position,
+        img: input.img,
+        note: input.note,
+      },
+    ]);
+
+    setInput({
+      img: input.img,
+      name: "",
+      position: "",
+      note: "",
+    });
+  };
+
+  return (
+    <>
+      <div className={showForm ? "team-done done-hide" : "team-done done-show"}>
+        <TeamDone />
+      </div>
+
+      <div
+        className={
+          showForm ? "list-form form-show" : "list-form inactive form-hide"
+        }
+      >
+        <h1>Create Your Winning Team!</h1>
+        <h2>
+          Within giving you the chance to build your own team (without any
+          limitation whatsoever!)..what your Elite Team would look like?
+        </h2>
+        <p>👇 Fill in the form below to add your top 5 team members:</p>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="name"
+          value={input.name}
+          placeholder="Name"
+          maxLength={maxlength}
+          required
+        />
+        <input
+          type="text"
+          onChange={handleChange}
+          name="position"
+          value={input.position}
+          placeholder="Position"
+          maxLength={maxlength}
+          required
+        />
+
+        <h4>Avatar:</h4>
+        <div className="radio-avatar">
+          <div>
+            <input
+              type="radio"
+              value={avatarUrl.young}
+              name="img"
+              onChange={handleChange}
+              defaultChecked={true}
+            />
+            <label htmlFor="Male">
+              <img className="thumb-avatar" alt="" src={avatarUrl.young} />
+            </label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              value={avatarUrl.female}
+              name="img"
+              onChange={handleChange}
+            />
+            <label htmlFor="Female">
+              {" "}
+              <img className="thumb-avatar" alt="" src={avatarUrl.female} />
+            </label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              value={avatarUrl.adult}
+              name="img"
+              onChange={handleChange}
+            />
+            <label htmlFor="Other">
+              <img className="thumb-avatar" alt="" src={avatarUrl.adult} />
+            </label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              value={avatarUrl.other}
+              name="img"
+              onChange={handleChange}
+            />
+            <label htmlFor="Other">
+              <img className="thumb-avatar" alt="" src={avatarUrl.other} />
+            </label>
+          </div>
+
+          <div>
+            <input
+              type="radio"
+              value={avatarUrl.unique}
+              name="img"
+              onChange={handleChange}
+            />
+            <label htmlFor="Other">
+              <img className="thumb-avatar" alt="" src={avatarUrl.unique} />
+            </label>
+          </div>
+        </div>
+
+        <textarea
+          onChange={handleChange}
+          name="note"
+          value={input.note}
+          placeholder="Short Anecdote"
+          maxLength={maxlength + 25}
+          required
+        />
+
+        <div className={error ? "formError" : "formError hide"}>
+          Please check your entries and try again.
+        </div>
+
+        <button onClick={checkForm}> Add New Team Member</button>
+      </div>
+    </>
+  );
+};
+
+export default FormList;
