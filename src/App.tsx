064413072c9e1323c1aa049b5cb76d148af99abd
Nirit Nagar <nirit.nn@gmail.com>
2023-02-14 22:41:05 +0100

    update avatars url path

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ export interface IState {
 }
 
 export const avatarUrl = {
-  young: "https://ibb.co/YfB4QnM",
-  female: "https://ibb.co/RTT2Dm3",
-  adult: "https://ibb.co/Wc6BTVx",
-  other: "https://ibb.co/sqSX2fb",
-  unique: "https://ibb.co/zfb7kpt",
+  young: "https://i.ibb.co/9yHmgKL/avatar5.png",
+  female: "https://i.ibb.co/TMMcgS4/avatar10.png",
+  adult: "https://i.ibb.co/CnzVrP1/avatar2.png",
+  other: "https://i.ibb.co/JHLSK1m/avatar4.png",
+  unique: "https://i.ibb.co/Lp6JGms/avatar13.png",
 };
 
 function App() {
